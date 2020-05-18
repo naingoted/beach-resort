a252a302606940bd10da36af9928f8eba79ff6b1
Vincent Oo <naingoted@gmail.com>
2020-05-18 09:11:34 +0800

    finished up the app and updated readme

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,8 +1,9 @@
 const contentful = require("contentful");
 
 export default contentful.createClient({
-  space: "czabv5x5utob",
-  accessToken:"BNnqt8qr8uRnPzU5Jr71pe00h9sMdKGHeKu4TRAwzWU",
+  // space: "48t1s0p1dk0p",
+  // accessToken:
+  //   "8bcdb00a5433f65ca9c9059c7ba3e3a7d0b3a2ccb293eb4a3c4af6951d9047a0"
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
